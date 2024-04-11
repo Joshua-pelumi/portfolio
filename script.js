f187d1ec29e6a73e1df94334ce11ffd48cce6615
Joshua-pelumi <joshuaogundairo9@gmail.com>
2024-04-11 21:42:53 +0100

    Added DOM Manipulation

---
+++ script.js
@@ -0,0 +1,9 @@
+window.alert('Welcome to my Portfolio Website')
+
+let changetext = document.getElementById('textonclick');
+
+function myFunction(){
+   changetext.innerHTML = 'You can reach out through the message section. Be sure we will get back to you' 
+}
+
+
